5f42f50eafcd183789ae3fe5e38454d5fec29bdb
vaishnavikathare <2023bit151@sggs.ac.in>
2025-03-23 15:47:32 +0530

    Create ISE2_CMS_program1.java

---
+++ ISE2_CMS_program1.java
@@ -0,0 +1,68 @@
+class Student{
+	String Name;
+	String Branch;
+	String dob;
+	double diplomaScore;
+	double mhtcetScore;
+	static int count=0;
+	Student()
+	{
+		count++;
+	}
+}
+
+
+class RegularStudent extends Student{
+	
+	public static void main(String args[])
+	{
+		RegularStudent rpm=new RegularStudent("R P Mathwale","IT","2003-08-12",95.5);
+		DSYStudent amf=new DSYStudent("A M Fajge","CSE","2003-05-12",95.5);
+		System.out.println("Total Number of Students: " +Student.count);
+		System.out.println("Total Number of Regular Students: " +RegularStudent.count);
+		System.out.println("Total Number of DSY Students: " +DSYStudent.count);
+		
+		RegularStudent bss=new RegularStudent("B S Shetty","IT","2003-11-03",99.5);
+		System.out.println("Total Number of Students: " +Student.count);
+		System.out.println("Total Number of Regular Students: " +RegularStudent.count);
+		System.out.println("Total Number of DSY Students: " +DSYStudent.count);
+		
+		System.out.println(amf);
+		System.out.println("------------------------------");
+		System.out.println(bss);
+		
+	
+	}
+	static int count=0;
+	RegularStudent(String Name, String Branch,String dob,double mhtcetScore)
+	{
+		this.Name=Name;
+		this.Branch=Branch;
+		this.dob=dob;
+		this.mhtcetScore=(double)mhtcetScore;
+		count++;
+		
+	}
+	public String toString()
+	{
+		return "Name: "+Name+"\nBranch: "+Branch+"\nDOB: "+dob+"\nMHTCET Score: "+mhtcetScore;	
+	}
+
+}
+	
+	
+class DSYStudent extends Student{
+	static int count=0;
+	DSYStudent(String Name, String Branch,String dob, double diplomaScore)
+	{
+		this.Name=Name;
+		this.Branch=Branch;
+		this.dob=dob;
+		this.diplomaScore=(double)diplomaScore;
+		count++;	
+	}
+	public String toString()
+	{
+		return "Name: "+Name+"\nBranch: "+Branch+"\nDOB: "+dob+"\nDiploma Score: "+diplomaScore;	
+	}
+ }
